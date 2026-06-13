accfdba9e460fd3259dbbe98d51268fea7b6f7b3
Mofeleng <mofelengmokgope@gmail.com>
2026-06-13 12:41:08 +0200

    Init Abi main app

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, IBM_Plex_Mono, IBM_Plex_Sans, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+const ibmPlex = IBM_Plex_Mono({
+  variable: "--font-plex-mono",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"]
+});
+
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-plex-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Abi - Your Automated Business Intelligence Specialist",
+  description: "Automate your entire data analysis pipeline in minutes. Connect your data sources, create a new workstation, and watch Abi analyse and present reports your shareholders will drool over.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", ibmPlex.variable, ibmPlexSans.variable, "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
