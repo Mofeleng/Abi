fbe3f79b10596ec99468df71cac262813d67202a
Mofeleng <mofelengmokgope@gmail.com>
2026-06-16 13:05:28 +0200

    Created chat ui and data sources ui

---
+++ app/src/app/dashboard/data/page.tsx
@@ -0,0 +1,6 @@
+import { DataSourcesView } from "@/modules/data/ui/views/data-sources-view";
+
+export default function Page() {
+
+    return <DataSourcesView />
+}
--- app/src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ app/src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -1,26 +1,35 @@
 "use client";
 
 import { Separator } from "@/components/ui/separator";
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
-import { BriefcaseBusinessIcon, DatabaseIcon } from "lucide-react";
+import { BlocksIcon, BriefcaseBusinessIcon, DatabaseIcon, GitGraphIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { DashboardUserButton } from "./dashboard-user-button";
 
 
-const sidebarItems = [
+const groupOne = [
     {
         icon: BriefcaseBusinessIcon,
         label: "Workspace",
         link: "/dashboard/workspace"
     },
+    
+]
+
+const groupTwo = [
     {
         icon: DatabaseIcon,
         label: "Data sources",
         link: "/dashboard/data"
     },
+    {
+        icon: BlocksIcon,
+        label: "Intergrations",
+        link: "/dashboard/intergrations"
+    }
 ]
 
 export function DashboardSidebar() {
@@ -39,9 +48,36 @@ export function DashboardSidebar() {
             <Separator />
             <SidebarContent>
                 <SidebarGroup>
+                    <SidebarGroupLabel>Work</SidebarGroupLabel>
                     <SidebarGroupContent>
                         <SidebarMenu>
-                            { sidebarItems.map((item) => (
+                            { groupOne.map((item) => (
+                                <SidebarMenuItem key={item.link}>
+                                    <SidebarMenuButton
+                                        asChild
+                                        className={cn(
+                                            "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-primary/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                                            pathname === item.link && "bg-linear-to-r/oklch border-primary/10"
+                                        )}
+                                        isActive={pathname === item.link}
+                                    >
+                                        <Link href={item.link}>
+                                            <item.icon className="size-5"/>
+                                            <span className="text-sm font-medium tracking-tight">
+                                                { item.label }
+                                            </span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Manage</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            { groupTwo.map((item) => (
                                 <SidebarMenuItem key={item.link}>
                                     <SidebarMenuButton
                                         asChild
