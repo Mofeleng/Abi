de2a2869908f56bb536e7a1dce854dc3c8ba688e
Mofeleng <mofelengmokgope@gmail.com>
2026-07-13 01:52:49 +0200

    Add pre ping and recycle

--- backend/packages/database/engine.py
+++ backend/packages/database/engine.py
@@ -7,5 +7,7 @@ load_dotenv()
 engine = create_async_engine(
     os.getenv("DATABASE_URL"),
     echo=True,
-    connect_args={"ssl":True}    
+    connect_args={"ssl":True},
+    pool_pre_ping=True, 
+    pool_recycle=300, 
 )
