8cd3517f2b4bc4996f5bbfaddcaee4bccd407542
Mofeleng <mofelengmokgope@gmail.com>
2026-07-13 01:58:22 +0200

    Added app to allow

--- backend/api/src/app.py
+++ backend/api/src/app.py
@@ -34,7 +34,8 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://abi-blond.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
