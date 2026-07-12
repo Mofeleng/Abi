36439dc5f8df732f333b69f467a18bbff98a0b0b
Mofeleng <mofelengmokgope@gmail.com>
2026-07-13 01:42:05 +0200

    Update engine and port

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,9 +1,15 @@
 import uvicorn
 import sys
 import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if sys.platform.startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
-    uvicorn.run("api.src.app:app", host="0.0.0.0", port=5000, log_level="info", reload=True)
+    uvicorn.run("api.src.app:app", host="0.0.0.0", port=port, log_level="info", reload=True)
--- backend/packages/database/engine.py
+++ backend/packages/database/engine.py
@@ -4,4 +4,8 @@ import os
 
 load_dotenv()
 
-engine = create_async_engine(os.getenv("DATABASE_URL"), echo=True)
+engine = create_async_engine(
+    os.getenv("DATABASE_URL"),
+    echo=True,
+    connect_args={"ssl":True}    
+)
